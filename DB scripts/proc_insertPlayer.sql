c0f7eace26f1a515bf3cc022bf1d83b916bb6ba6
Tomas-Mas <120631553+Tomas-Mas@users.noreply.github.com>
2023-03-25 09:42:31 +0100

    Update proc_insertPlayer.sql

--- DB scripts/proc_insertPlayer.sql
+++ DB scripts/proc_insertPlayer.sql
@@ -24,13 +24,13 @@ begin
     savepoint insertPlayer_start;
     --get t_id by json id if team exists, null if not
     select COALESCE(MAX(t_id), null) into dTeamId from teams where t_jsonId = teamJsonId;
-    
+
     --position
     if positionExist(positionName) = false then
         insert into positions(name, type, abbreviation) values(positionName, positionType, positionAbbreviation);
     end if;
     select p_id into dPositionId from positions where name = positionName;
-    
+
     --player todo age
     if playerExist(pJsonId) then
         select p_id, p_jsonId, firstName, lastName, primaryNumber, age, birthDate, birthCountry, currentTeamId, positionId
@@ -54,7 +54,7 @@ begin
         insert into players(p_jsonId, firstName, lastName, primaryNumber, birthDate, birthCountry, currentTeamId, positionId)
             values(pJsonId, pFirstName, pLastName, pPrimaryNumber, to_date(pBirthDate, 'YYYY-MM-DD'), pBirthCountry, dTeamId, dPositionId);
     end if;
-    
+
     --rosters
     select g_id into dRosterGameId from games where g_jsonId = pGameId;
     if pFirstName = 'Unknown' and pLastName = 'Unknown' then
@@ -66,17 +66,9 @@ begin
     if rosterExist(dRosterGameId, dRosterTeamId, dRosterPlayerId) = false then
         insert into Rosters(g_id, t_id, p_id) values (dRosterGameId, dRosterTeamId, dRosterPlayerId);
     end if;
-    
+
 exception
     when others then
     rollback to insertPlayer_start;
     raise;
 end;
-
-execute insertPlayer(2015020704, 24, 8471699, 'Andrew', 'Cogliano', 11, '1987-06-14', 'CAN', 28, 'Center', 'Forward', 'C');  
-
---execute insertPlayer(8476116, 'Ryan', 'Garbutt', 16, '1985-08-12', 'CAN', null, null, 'Center', 'Forward', 'C');
-
-
-
---[8471699, Andrew, Cogliano, 11, 1987-06-14, CAN, 28, San Jose Sharks, Center, Forward, C]
