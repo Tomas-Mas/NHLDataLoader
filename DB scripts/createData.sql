edf3a08b07e8fdee93207d7833942bf70cf933f7
Tomas-Mas <120631553+Tomas-Mas@users.noreply.github.com>
2023-03-25 09:45:23 +0100

    Update createData.sql

--- DB scripts/createData.sql
+++ DB scripts/createData.sql
@@ -1,10 +1,27 @@
 --creating tables
 CREATE TABLE Conference_Division
   (
-    Conferences_c_id INTEGER NOT NULL ,
-    Divisions_d_id   INTEGER NOT NULL
+    conference_id INTEGER NOT NULL ,
+    division_id   INTEGER NOT NULL ,
+    season integer not null
   ) ;
-ALTER TABLE Conference_Division ADD CONSTRAINT Conference_Division_PK PRIMARY KEY ( Conferences_c_id, Divisions_d_id );
+ALTER TABLE Conference_Division ADD CONSTRAINT Conference_Division_PK PRIMARY KEY ( conference_id, division_id, season );
+
+create table Division_Teams (
+    dt_id integer not null,
+    division_id integer not null,
+    team_id integer not null,
+    season integer not null
+);
+alter table Division_Teams add constraint Division_Teams_PK primary key (dt_id);
+
+create table Conference_Teams (
+    ct_id integer not null,
+    conference_id integer not null,
+    team_id integer not null,
+    season integer not null
+);
+alter table conference_Teams add constraint Conference_Teams_PK primary key (ct_id);
 
 CREATE TABLE Conferences
   (
@@ -26,7 +43,7 @@ CREATE TABLE Games
   (
     g_id       INTEGER NOT NULL ,
     g_jsonId   INTEGER NOT NULL ,
-    gameType   VARCHAR2 (1 CHAR) NOT NULL ,
+    gameType   VARCHAR2(5) NOT NULL ,
     season     INTEGER NOT NULL ,
     gameDate   DATE NOT NULL ,
     awayScore  INTEGER NOT NULL ,
@@ -34,32 +51,9 @@ CREATE TABLE Games
     homeScore  INTEGER NOT NULL ,
     homeTeamId INTEGER NOT NULL ,
     venueId    INTEGER NOT NULL ,
-    gameStatus Varchar2(15)
+    gameStatus INTEGER NOT NULL
   );
 ALTER TABLE Games ADD CONSTRAINT Games_PK PRIMARY KEY ( g_id );
-
-CREATE TABLE GoalEvents
-  (
-    g_id         INTEGER NOT NULL ,
-    gameId       INTEGER NOT NULL ,
-    emptyNet     VARCHAR2 (5) NOT NULL ,
-    strength     VARCHAR2 (25) NOT NULL ,
-    strengthCode VARCHAR2 (10) ,
-    period       INTEGER NOT NULL ,
-    periodType   VARCHAR2 (15) NOT NULL ,
-    ordinalNum   VARCHAR2 (5) ,
-    periodTime   VARCHAR2(6) NOT NULL ,
-    teamId       INTEGER NOT NULL
-  );
-ALTER TABLE GoalEvents ADD CONSTRAINT GoalEvents_PK PRIMARY KEY ( g_id );
-
-CREATE TABLE Goal_Players
-  (
-    GoalEvents_g_id INTEGER NOT NULL ,
-    Players_p_id    INTEGER NOT NULL ,
-    role            VARCHAR2 (10) NOT NULL
-  );
-ALTER TABLE Goal_Players ADD CONSTRAINT Goal_Players_PK PRIMARY KEY ( GoalEvents_g_id, Players_p_id );
 
 CREATE TABLE Players
   (
@@ -67,12 +61,11 @@ CREATE TABLE Players
     p_jsonId      INTEGER NOT NULL ,
     firstName     VARCHAR2 (25) NOT NULL ,
     lastName      VARCHAR2 (25) NOT NULL ,
-    "NUMBER"	  INTEGER ,
+    primaryNumber INTEGER ,
+    age           integer,
     birthDate     DATE ,
     birthCountry  VARCHAR2 (3) ,
-    active        VARCHAR2 (5) NOT NULL ,
-    rosterStatus  VARCHAR2 (1) ,
-    currentTeamId INTEGER NOT NULL ,
+    currentTeamId INTEGER ,
     positionId    INTEGER NOT NULL
   );
 ALTER TABLE Players ADD CONSTRAINT Players_PK PRIMARY KEY ( p_id );
@@ -112,6 +105,13 @@ CREATE TABLE TimeZones
   );
 ALTER TABLE TimeZones ADD CONSTRAINT TimeZones_PK PRIMARY KEY ( tz_id );
 
+CREATE TABLE GameStatus
+  (
+    code INTEGER NOT NULL,
+    name VARCHAR2 (15)
+  );
+ALTER TABLE GameStatus ADD CONSTRAINT GameStatus_PK PRIMARY KEY ( code );
+
 CREATE TABLE Venues
   (
     v_id INTEGER NOT NULL ,
@@ -120,35 +120,101 @@ CREATE TABLE Venues
   );
 ALTER TABLE Venues ADD CONSTRAINT Venues_PK PRIMARY KEY ( v_id );
 
+create table Rosters(
+    r_id integer not null,
+    g_id integer not null,
+    t_id integer,
+    p_id integer not null,
+    ss_id integer,
+    gs_id integer
+);
+alter table Rosters add constraint Rosters_PK primary key(r_id);
+
+create table Events(
+    e_id integer not null,
+    name varchar2(15),
+    secondaryType varchar2(50),
+    strength varchar2(20),
+    emptyNet varchar2(10),
+    penaltySeverity varchar2(30),
+    penaltyMinutes integer
+);
+alter table Events add constraint Events_PK primary key(e_id);
+
+create table GameEvents(
+    ge_id integer not null,
+    gameId integer not null,
+    gameEventId integer not null,
+    eventId integer not null,
+    periodNumber integer not null,
+    periodType varchar2(15) not null,
+    periodTime varchar2(10) not null,
+    coordX integer,
+    coordY integer
+);
+alter table GameEvents add constraint GameEvents_PK primary key(ge_id);
+
+create table EventPlayers(
+    event_id integer not null,
+    roster_id integer not null,
+    role varchar2(50)
+);
+alter table EventPlayers add constraint EventPlayers_PK primary key(event_id, roster_id);
+
+create table SkaterStats(
+    ss_id integer not null,
+    timeOnIce varchar2(10) not null,
+    ppTimeOnIce varchar2(10) not null,
+    shTimeOnIce varchar2(10) not null,
+    penaltyMinutes integer not null,
+    plusMinus integer not null
+);
+alter table SkaterStats add constraint SkaterStats_PK primary key(ss_id);
+
+create table GoalieStats(
+    gs_id integer not null,
+    timeOnIce varchar2(10) not null,
+    penaltyMinutes integer not null,
+    shots integer not null,
+    saves integer not null,
+    ppShots integer not null,
+    ppSaves integer not null,
+    shShots integer not null,
+    shSaves integer not null
+);
+alter table GoalieStats add constraint GoalieStats_PK primary key(gs_id);
+
+insert into venues (v_id, name, city) Values(0, 'Unknown', 'N/A');
 
 --fk constraints
-ALTER TABLE Conference_Division ADD CONSTRAINT Conference_Div_Con_FK FOREIGN KEY ( Conferences_c_id ) REFERENCES Conferences ( c_id );
-ALTER TABLE Conference_Division ADD CONSTRAINT Con_Div_Division_FK FOREIGN KEY ( Divisions_d_id ) REFERENCES Divisions ( d_id );
+ALTER TABLE Conference_Division ADD CONSTRAINT Conference_Div_Con_FK FOREIGN KEY ( conference_id ) REFERENCES Conferences ( c_id );
+ALTER TABLE Conference_Division ADD CONSTRAINT Con_Div_Division_FK FOREIGN KEY ( division_id ) REFERENCES Divisions ( d_id );
+alter table Division_Teams add constraint Division_Teams_Divisions_FK foreign key (division_id) references Divisions (d_id);
+alter table Division_Teams add constraint Division_Teams_Teams_FK foreign key (team_id) references Teams (t_id) INITIALLY DEFERRED DEFERRABLE;
+alter table Conference_Teams add constraint Conference_Teams_Conferences_FK foreign key (conference_id) references Conferences (c_id);
+alter table Conference_Teams add constraint Conference_Teams_Teams_FK foreign key (team_id) references Teams (t_id) INITIALLY DEFERRED DEFERRABLE;
 ALTER TABLE Games ADD CONSTRAINT Games_Teams_FK_Away FOREIGN KEY ( awayTeamId ) REFERENCES Teams ( t_id );
 ALTER TABLE Games ADD CONSTRAINT Games_Teams_FK_Home FOREIGN KEY ( homeTeamId ) REFERENCES Teams ( t_id );
+ALTER TABLE Games ADD CONSTRAINT Games_GameStatus_FK FOREIGN KEY ( gameStatus ) REFERENCES GameStatus ( code );
 ALTER TABLE Games ADD CONSTRAINT Games_Venues_FK FOREIGN KEY ( venueId ) REFERENCES Venues ( v_id );
-ALTER TABLE GoalEvents ADD CONSTRAINT GoalEvents_Games_FK FOREIGN KEY ( gameId ) REFERENCES Games ( g_id );
-ALTER TABLE GoalEvents ADD CONSTRAINT GoalEvents_Teams_FK FOREIGN KEY ( teamId ) REFERENCES Teams ( t_id );
-ALTER TABLE Goal_Players ADD CONSTRAINT Goal_Players_GoalEvents_FK FOREIGN KEY ( GoalEvents_g_id ) REFERENCES GoalEvents ( g_id );
-ALTER TABLE Goal_Players ADD CONSTRAINT Goal_Players_Players_FK FOREIGN KEY ( Players_p_id ) REFERENCES Players ( p_id );
 ALTER TABLE Players ADD CONSTRAINT Players_Positions_FK FOREIGN KEY ( positionId ) REFERENCES Positions ( p_id );
 ALTER TABLE Players ADD CONSTRAINT Players_Teams_FK FOREIGN KEY ( currentTeamId ) REFERENCES Teams ( t_id );
-ALTER TABLE Teams ADD CONSTRAINT Teams_Conferences_FK FOREIGN KEY ( conferenceId ) REFERENCES Conferences ( c_id );
-ALTER TABLE Teams ADD CONSTRAINT Teams_Divisions_FK FOREIGN KEY ( divisionId ) REFERENCES Divisions ( d_id );
+alter table Teams add constraint Teams_Division_Teams_FK foreign key(divisionId) references Division_Teams(dt_id) INITIALLY DEFERRED DEFERRABLE;
+alter table Teams add constraint Teams_Conference_Teams_FK foreign key(conferenceId) references Conference_Teams(ct_id) INITIALLY DEFERRED DEFERRABLE;
 ALTER TABLE Teams ADD CONSTRAINT Teams_TimeZones_FK FOREIGN KEY ( timeZoneId ) REFERENCES TimeZones ( tz_id );
 ALTER TABLE Teams ADD CONSTRAINT Teams_Venues_FK FOREIGN KEY ( venueId ) REFERENCES Venues ( v_id );
+alter table Rosters add constraint Rosters_Games_FK foreign key (g_id) references Games (g_id);
+alter table Rosters add constraint Rosters_Teams_FK foreign key (t_id) references Teams (t_id);
+alter table Rosters add constraint Rosters_Players_FK foreign key (p_id) references Players (p_id);
+alter table GameEvents add constraint GameEvents_Games_FK foreign key(gameId) references Games(g_id);
+alter table EventPlayers add constraint EventPlayers_GameEvents_FK foreign key(event_id) references GameEvents (ge_id);
+alter table EventPlayers add constraint EventPlayers_Rosters_FK foreign key(roster_id) references Rosters(r_id);
+alter table GameEvents add constraint GameEvents_Events_FK foreign key(eventId) references Events(e_id);
+alter table Rosters add constraint Rosters_SkaterStats_FK foreign key(ss_id) references SkaterStats(ss_id);
+alter table Rosters add constraint Rosters_GoalieStats_FK foreign key(gs_id) references GoalieStats(gs_id);
+
 
 --create sequences
-drop sequence seq_division;
-drop sequence seq_conference;
-drop sequence seq_tZone;
-drop sequence seq_position;
-drop sequence seq_team;
-drop sequence seq_venue;
-drop sequence seq_player;
-drop sequence seq_goalEvnt;
-drop sequence seq_game;
-
 CREATE SEQUENCE seq_division
 START WITH 1
 INCREMENT BY 1
@@ -159,6 +225,8 @@ NOCYCLE;
 CREATE OR REPLACE TRIGGER division_d_id_TRG BEFORE
   INSERT ON Divisions FOR EACH ROW WHEN (NEW.d_id IS NULL) BEGIN :NEW.d_id := seq_division.NEXTVAL;
 END;
+
+/
 
 CREATE SEQUENCE seq_conference
 START WITH 30
@@ -171,27 +239,59 @@ CREATE OR REPLACE TRIGGER conference_c_id_TRG BEFORE
   INSERT ON Conferences FOR EACH ROW WHEN (NEW.c_id IS NULL) BEGIN :NEW.c_id := seq_conference.NEXTVAL;
 END;
 
+/
+
+create sequence seq_divisionTeams
+start with 1
+increment by 1
+minvalue 1
+maxvalue 1000
+nocache
+nocycle;
+/*CREATE OR REPLACE TRIGGER divisionTeams_dt_id_TRG BEFORE
+  INSERT ON Division_Teams FOR EACH ROW WHEN (NEW.dt_id IS NULL) BEGIN :NEW.dt_id := seq_divisionTeams.NEXTVAL;
+END;*/
+
+/
+
+create sequence seq_conferenceTeams
+start with 1
+increment by 1
+minvalue 1
+maxvalue 1000
+nocache
+nocycle;
+/*create or replace trigger conferenceTeams_ct_id_TRG before
+    insert on Conference_Teams for each row when (new.ct_id is null) begin :new.ct_id := seq_conferenceTeams.nextval;
+end;*/
+
+/
+
 CREATE SEQUENCE seq_tZone
-START WITH 50
+START WITH 1
 INCREMENT BY 1
-MINVALUE 50
-MAXVALUE 59
+MINVALUE 1
+MAXVALUE 1000
 NOCACHE
 NOCYCLE;
 CREATE OR REPLACE TRIGGER timeZone_tz_id_TRG BEFORE
   INSERT ON TimeZones FOR EACH ROW WHEN (NEW.tz_id IS NULL) BEGIN :NEW.tz_id := seq_tZone.NEXTVAL;
 END;
 
+/
+
 CREATE SEQUENCE seq_position
-START WITH 60
+START WITH 100
 INCREMENT BY 1
-MINVALUE 60
-MAXVALUE 79
+MINVALUE 100
+MAXVALUE 1000
 NOCACHE
 NOCYCLE;
 CREATE OR REPLACE TRIGGER position_p_id_TRG BEFORE
   INSERT ON Positions FOR EACH ROW WHEN (NEW.p_id IS NULL) BEGIN :NEW.p_id := seq_position.NEXTVAL;
 END;
+
+/
 
 CREATE SEQUENCE seq_team
 START WITH 100
@@ -200,9 +300,11 @@ MINVALUE 100
 MAXVALUE 499
 NOCACHE
 NOCYCLE;
-CREATE OR REPLACE TRIGGER team_t_id_TRG BEFORE
+/*CREATE OR REPLACE TRIGGER team_t_id_TRG BEFORE
   INSERT ON Teams FOR EACH ROW WHEN (NEW.t_id IS NULL) BEGIN :NEW.t_id := seq_team.NEXTVAL;
-END;
+END;*/
+
+/
 
 CREATE SEQUENCE seq_venue
 START WITH 500
@@ -214,6 +316,9 @@ NOCYCLE;
 CREATE OR REPLACE TRIGGER venue_v_id_TRG BEFORE
   INSERT ON Venues FOR EACH ROW WHEN (NEW.v_id IS NULL) BEGIN :NEW.v_id := seq_venue.NEXTVAL;
 END;
+--drop sequence seq_venue;
+
+/
 
 CREATE SEQUENCE seq_player
 START WITH 1000
@@ -226,24 +331,69 @@ CREATE OR REPLACE TRIGGER player_p_id_TRG BEFORE
   INSERT ON Players FOR EACH ROW WHEN (NEW.p_id IS NULL) BEGIN :NEW.p_id := seq_player.NEXTVAL;
 END;
 
---CREATE SEQUENCE seq_goalEvnt
---START WITH 50000
---INCREMENT BY 1
---MINVALUE 50000
---MAXVALUE 199999
---CACHE 30
---NOCYCLE;
---CREATE OR REPLACE TRIGGER goalevnt_g_id_TRG BEFORE
---  INSERT ON GoalEvents FOR EACH ROW WHEN (NEW.g_id IS NULL) BEGIN :NEW.g_id := seq_goalEvnt.NEXTVAL;
---END;
+/
+
+create sequence seq_roster
+start with 1
+increment by 1
+minvalue 1
+maxvalue 9999999999
+cache 40
+nocycle;
+create or replace trigger roster_r_id_TRG before
+    insert on Rosters for each row when (new.r_id is null) begin :NEW.r_id := seq_roster.nextval;
+end;
+
+/
+
+
+CREATE SEQUENCE seq_gameEvents
+START WITH 1
+INCREMENT BY 1
+MINVALUE 1
+MAXVALUE 9999999999
+CACHE 50
+NOCYCLE;
+
+/
+
+CREATE SEQUENCE seq_events
+START WITH 1
+INCREMENT BY 1
+MINVALUE 1
+MAXVALUE 10000
+NOCACHE
+NOCYCLE;
+
+/
 
 CREATE SEQUENCE seq_game
 START WITH 200000
 INCREMENT BY 1
 MINVALUE 200000
 NOMAXVALUE 
-CACHE 20
+CACHE 5
 NOCYCLE;
 CREATE OR REPLACE TRIGGER game_g_id_TRG BEFORE
   INSERT ON Games FOR EACH ROW WHEN (NEW.g_id IS NULL) BEGIN :NEW.g_id := seq_game.NEXTVAL;
 END;
+
+/
+
+create sequence seq_skaterStats
+start with 1
+increment by 1
+minvalue 1
+maxvalue 99999999999
+cache 20
+nocycle;
+
+/
+
+create sequence seq_goalieStats
+start with 1
+increment by 1
+minvalue 1
+maxvalue 9999999999
+cache 5
+nocycle;
